3002f816bafa039c27bdd8b1c642b65ab85583bd
Frankline Isaboke <isabokefrankline@gmail.com>
2024-01-26 13:01:11 +0300

    modified the homepage and navigation bar

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Testimonials from "./Testimonials.jsx";
+import Hero from "./Hero.jsx";
+import Footer from "./Footer.jsx";
 
 function Home() {
   return (
     <div>
-      <h1 className="ui raised segment center aligned">Welcome</h1>
+      <h1>Welcome to BiteBinders</h1>
+      <Hero />
       <Testimonials />
+      <Footer />
     </div>
   );
 }
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import MealApp from "./MealApp";
-import SignIn from "./SignIn";
-import SignUp from "./SignUp";
-import "/src/index.css"
+import "/src/index.css";
 
 const NavBar = () => (
-  <nav className="bg-blue-800 p-2">
+  <nav className="ui raised segment center aligned">
     <ul className="flex justify-between text-white">
       <li className="mx-2">
         <Link to="/" className="hover:underline">
@@ -15,17 +12,12 @@ const NavBar = () => (
       </li>
       <li className="mx-2">
         <Link to="/mealapp" className="hover:underline">
-          MealApp
+          Meals
         </Link>
       </li>
       <li className="mx-2">
         <Link to="/signin" className="hover:underline">
           Sign In
-        </Link>
-      </li>
-      <li className="mx-2">
-        <Link to="/signup" className="hover:underline">
-          Sign Up
         </Link>
       </li>
     </ul>
