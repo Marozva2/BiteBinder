02965faba2f2f5736df2ce0e35677a0a34c3e213
Frankline Isaboke <isabokefrankline@gmail.com>
2024-01-28 18:34:46 +0300

    renamed files

--- server/routes/instruction.py
+++ server/routes/instruction.py
@@ -2,13 +2,18 @@ from flask_restful import Resource, reqparse
 from models import db, RecipeInstructions
 from schema import RecipeInstructionsSchema
 from flask import jsonify
+import requests
 
 recipe_instructions_schema = RecipeInstructionsSchema()
 recipe_instructions_list_schema = RecipeInstructionsSchema(many=True)
 
+apiKey = 'a0fb34d2e4204a8a9ae9e15588519efc'
+
 class RecipeInstructionsResource(Resource):
     def get(self, instruction_id):
-        instruction = RecipeInstructions.query.get_or_404(instruction_id)
+        response = requests.get(f'https://api.spoonacular.com/recipes/{instruction_id}/analyzedInstructions?apiKey={apiKey}')
+        data = response.json()
+        instruction = RecipeInstructions(detail=data[0]['steps'][0]['step'])
         return jsonify(recipe_instructions_schema.dump(instruction))
 
     def put(self, instruction_id):
@@ -17,7 +22,6 @@ class RecipeInstructionsResource(Resource):
         parser.add_argument('detail', type=str)
         args = parser.parse_args()
 
-        # Update instruction attributes
         instruction.detail = args['detail'] or instruction.detail
 
         db.session.commit()
@@ -31,7 +35,9 @@ class RecipeInstructionsResource(Resource):
 
 class RecipeInstructionsListResource(Resource):
     def get(self):
-        instructions = RecipeInstructions.query.all()
+        response = requests.get(f'https://api.spoonacular.com/recipes/complexSearch?apiKey={apiKey}')
+        data = response.json()
+        instructions = [RecipeInstructions(detail=recipe['title']) for recipe in data['results']]
         return jsonify(recipe_instructions_list_schema.dump(instructions))
 
     def post(self):
--- server/routes/recipe.py
+++ server/routes/recipe.py
@@ -2,13 +2,18 @@ from flask_restful import Resource, reqparse
 from models import db, Recipe
 from schema import RecipeSchema
 from flask import jsonify
+import requests
 
 recipe_schema = RecipeSchema()
 recipes_schema = RecipeSchema(many=True)
 
+apiKey = 'a0fb34d2e4204a8a9ae9e15588519efc'
+
 class RecipeResource(Resource):
     def get(self, recipe_id):
-        recipe = Recipe.query.get_or_404(recipe_id)
+        response = requests.get(f'https://api.spoonacular.com/recipes/{recipe_id}/information?apiKey={apiKey}')
+        data = response.json()
+        recipe = Recipe(title=data['title'], description=data['instructions'])
         return jsonify(recipe_schema.dump(recipe))
 
     def put(self, recipe_id):
@@ -18,7 +23,6 @@ class RecipeResource(Resource):
         parser.add_argument('description', type=str)
         args = parser.parse_args()
 
-        # Update recipe attributes
         recipe.title = args['title'] or recipe.title
         recipe.description = args['description'] or recipe.description
 
@@ -33,7 +37,9 @@ class RecipeResource(Resource):
 
 class RecipesResource(Resource):
     def get(self):
-        recipes = Recipe.query.all()
+        response = requests.get(f'https://api.spoonacular.com/recipes/complexSearch?apiKey={apiKey}')
+        data = response.json()
+        recipes = [Recipe(title=recipe['title'], description=recipe['instructions']) for recipe in data['results']]
         return jsonify(recipes_schema.dump(recipes))
 
     def post(self):
